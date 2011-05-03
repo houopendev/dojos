8f2bece4f2ba4f0e6262eaf597daec1b5cdf212c
Michael Koby <michael@mkoby.com>
2011-05-02 20:19:24 -0500

    Added Card and Hand classes, per Peter's advice

---
+++ poker_hands/test/card_test.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+require '../lib/card'
+
+class TestCard < Test::Unit::TestCase
+
+  def test_truth #Example test, not a real test
+    assert true
+  end
+
+end
---
+++ poker_hands/test/hand_test.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+require '../lib/hand'
+
+class TestHand < Test::Unit::TestCase
+
+  def test_truth #Example test, not a real test
+    assert true
+  end
+
+end
