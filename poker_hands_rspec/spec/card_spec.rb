ba4b489d7b63457bbcebd83f072ce995f0b6074d
Michael Koby <michael@mkoby.com>
2011-05-14 20:16:29 -0500

    Finished all the original Test::Unit tests and added InvalidHand error

--- poker_hands_rspec/spec/card_spec.rb
+++ poker_hands_rspec/spec/card_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 class Card
   describe Card do
-    context "#new" do
+    describe "#new" do
       describe "Two of Hearts" do
         it "has a value of 2" do
           card = Card.new("2H")
@@ -28,6 +28,12 @@ class Card
         it "has a value of 11" do
           card = Card.new("JC")
           card.value.should equal(11)
+        end
+      end
+
+      describe "Bad Value" do
+        it "should raise StandardError" do
+          expect { card = Card.new("ZZ") }.to raise_error(StandardError)
         end
       end
     end
